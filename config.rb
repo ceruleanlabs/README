a823339c3aee96f21b2d829a90411c14ad317bfb
Dan Bergren <bergren2@gmail.com>
2015-10-06 23:01:03 -0500

    this one was going to bite me later on too

--- config.rb
+++ config.rb
@@ -66,6 +66,7 @@ configure :build do
 
   # Use relative URLs
   activate :relative_assets
+  set :relative_links, true
 
   # Or use a different image path
   # set :http_prefix, "/Content/images/"
