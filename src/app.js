5e050b28a803bc1143f9ee30792892d110bbdcc0
Kevin Pavao <kpavao84@gmail.com>
2017-11-05 01:53:40 -0600

    Use a grid to layout the app like the original.

--- src/app.js
+++ src/app.js
@@ -1,21 +1,36 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { withStyles } from 'material-ui/styles';
+import Grid from 'material-ui/Grid';
 import FullWidthTabs from './components/tabs';
 import TextFields from './components/fields';
 import Header from './components/header';
 
-function App() {
+const styles = theme => ({
+  root: {
+    display: 'flex',
+    marginRight: 100,
+    marginLeft: 100
+  }
+});
+
+function App(props) {
+  const { classes } = props;
   return (
-    <div>
-      <Header />
-      <div>
-        <TextFields />
-      </div>
-      <div>
-        <FullWidthTabs />
-      </div>
+    <div className={classes.root}>
+      <Grid container spacing={24}>
+        <Grid item xs={12}>
+          <Header />
+        </Grid>
+        <Grid item xs={6}>
+          <TextFields />
+        </Grid>
+        <Grid item xs={6}>
+          <FullWidthTabs />
+        </Grid>
+      </Grid>
     </div>
   );
 }
 
-export default App;
+export default withStyles(styles)(App);
