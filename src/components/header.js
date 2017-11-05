391ffe110f7ce911fad6595ed585277026a57e1a
Kevin Pavao <kpavao84@gmail.com>
2017-11-05 02:14:39 -0600

    Use typography component for header, get roboto from the cdn

--- src/components/header.js
+++ src/components/header.js
@@ -1,15 +1,18 @@
 import React from 'react';
+import Typography from 'material-ui/Typography';
 
 function Header() {
   return (
     <div>
-      <h1>README</h1>
-      <p>
-      README is a <em>common documentation generator.</em> Its purpose is to quickly generate documentation for a project so you can jump right into development knowing the core of your project is already documented for you.
-      </p>
-      <p>
-      It primarily supports <em>quick, simple projects</em> for epically bootstrapped documentation.
-      </p>
+      <Typography type="display3" gutterBottom>
+        README
+      </Typography>
+      <Typography type="body2">
+        <strong>README</strong> is a <em>common documentation generator.</em> Its purpose is to quickly generate documentation for a project so you can jump right into development knowing the core of your project is already documented for you.
+      </Typography>
+      <Typography type="body2">
+        It primarily supports <em>quick, simple projects</em> for epically bootstrapped documentation.
+      </Typography>
     </div>
   );
 }
